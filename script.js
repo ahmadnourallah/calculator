978bcb9d6ef742fb5318ce27b4093048c7cf341c
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-10-09 13:25:42 -0400

    Create the logic of carrying out operations

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function multiply(operand1, operand2) {
 function divide(operand1, operand2) {
     return operand1 / operand2;
 }
+
+function operator(operand1, operand2, operator) {
+    if (operator === "add") {
+        return add(operand1, operand2);
+    
+    } else if (operator === "subtract") {
+        return subtract(operand1, operand2);
+    
+    } else if (operator === "multiply") {
+        return multiply(operand1, operand2);
+
+    } else if (operator === "divide") {
+        return divide(operand1, operand2);
+
+    }
+}
