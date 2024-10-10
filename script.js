9b203c6395c6197c2d56c9cf20e0390e757d29f3
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-10-10 08:07:34 -0400

    Remove data tag from number button as text content suffices

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ function operator(operand1, operand2, operator) {
 
 document.querySelectorAll(".number-btn").forEach(btn => {
     btn.addEventListener("click", (event) => {
-        if (!(event.target.dataset.value === "." && displayCurrentOperand.textContent.includes("."))) {
-            displayCurrentOperand.textContent += event.target.dataset.value;
+        if (!(event.target.textContent === "." && displayCurrentOperand.textContent.includes("."))) {
+            displayCurrentOperand.textContent += event.target.textContent;
 
         }
     });
